a4f09b27a1fe7e24262981000b4b9e8ecbe809af
edvardhua <edvard_hua@live.com>
2019-10-25 12:17:00 +0800

    minor revision

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 cd dl_train
 docker build -t dl_train .
+wget https://archive.apache.org/dist/hadoop/core/hadoop-2.7.4/hadoop-2.7.4.tar.gz -P dl_train_hdfs/
 cd ../dl_train_hdfs
 docker build -t dl_train_hdfs .
